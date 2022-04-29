98ffe0225a413efb4abf2731e5724ed2afe52253
rheyavlan <rheya.vithlani@gmail.com>
2022-04-29 17:49:06 -0400

    Initial commit

---
+++ carrental/includes/footer.php
@@ -0,0 +1,7 @@
+<footer id="footer">
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong>PDS Project by NYU Students</strong>. All Rights Reserved
+    </div>
+  </div>
+</footer>
